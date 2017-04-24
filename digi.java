8da5ba86357e50e0ca43261bd94dfb95cb5d0b3c
kaaruR <karthikaram@gmail.com>
2017-04-24 18:00:32 +0530

    Add files via upload

--- digi.java
+++ digi.java
@@ -7,6 +7,8 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
 int i=0;
+if(a>=-999999999 && a<=999999999)
+{
 while(a!=0)
 {
 int r=a%10;
@@ -14,5 +16,11 @@ a=a/10;
 i++;
 }
 System.out.println("no of digits:"+i);
+}
+
+else
+{
+System.out.println("enter a valid no from -999999999 to 999999999");
+}
 }
 }

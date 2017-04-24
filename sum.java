632ab5098596022bbbe78db09a24101ab2b4e5aa
kaaruR <karthikaram@gmail.com>
2017-04-24 17:31:48 +0530

    Add files via upload

--- sum.java
+++ sum.java
@@ -8,10 +8,17 @@ Scanner sc=new Scanner(System.in);
 System.out.println("enter any number:");
 int n=sc.nextInt();
 int sum=0;
+if(n>0)
+{
 for(int i=1;i<=n;i++)
 {
 sum=sum+i;
 }
 System.out.println(sum);
+}
+else
+{
+System.out.println("enter a valid natural number");
+}
 }
 }

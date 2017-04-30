08972f2508f68e7204c8beb8249dd99c9c14d15a
kaaruR <karthikaram@gmail.com>
2017-04-30 13:11:45 +0530

    Add files via upload

--- fact.java
+++ fact.java
@@ -7,11 +7,18 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 System.out.println("enter a number");
 int n=sc.nextInt();
+if(n>=0)
+{
 int fact=1;
 for(int i=1;i<=n;i++)
 {
 fact=fact*i;
 }
 System.out.println("factorial:"+fact);
+}
+else
+{
+System.out.println("enter a vaid number");
+}
 }
 }

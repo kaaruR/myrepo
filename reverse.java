a38cd33faa6805a893100d09f0ae3b2efcf373bb
kaaruR <karthikaram@gmail.com>
2017-04-30 12:37:09 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -5,17 +5,9 @@ public class reverse
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
+System.out.println("enter a string");
 String sb=sc.next();
-System.out.println(sb);
 String reverse = new StringBuffer(sb).reverse().toString();
-System.out.println(reverse);
-if(sb.equals(reverse))
-{
-System.out.println("palindrome");
-}
-else
-{
-System.out.println("not a palindrome");
-}
+System.out.println("reversed string:"+reverse);
 }
 }
